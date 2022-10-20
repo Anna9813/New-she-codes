fc6a718177020f2c1e4927053d4082ba8a5520db
Anna9813 <108585944+Anna9813@users.noreply.github.com>
2022-10-20 17:41:36 +0300

    Display the forecast data using the API

--- main.js
+++ main.js
@@ -13,6 +13,49 @@ function formateDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    //let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+        forecast.forEach(function (forecastDay, index) {
+            if (index < 6) {
+        forecastHTML = forecastHTML + 
+        `
+        <div class="col-2">
+            <div class="weather-forecast-date" id="forecast">${formatDay(forecastDay.dt)}</div>
+            <img 
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+                alt=""
+                width="42"
+            />
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+           </div>
+
+        </div>
+ `;
+    }
+});
+ forecastHTML = forecastHTML + `</div>`;
+ forecastElement.innerHTML = forecastHTML;          
+}
+
+function getForecast(coordinates) {
+    let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 //Функция изменения на странице температуры, города, влажности, ветра, даты, иконок
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -37,7 +80,10 @@ function displayTemperature(response) {
         );
     iconElement.setAttribute(
         "alt",  response.data.weather[0].description);
+
+        getForecast(response.data.coord);
 }
+
 //Функция изменения города
 function search(city) {
     let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
